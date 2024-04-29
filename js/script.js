7ee05852663e03fdec5ce07cdcb3979ea64e1b5a
George Khalil <159185251+George-Khalil-29@users.noreply.github.com>
2024-04-29 18:49:40 +0000

    save

--- js/script.js
+++ js/script.js
@@ -1,5 +1,24 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 George, Khalil All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: George, Khalil
+// Created on: Apr 2024
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * This function calculates area of a triangle.
+ */
+function pickAPzza() {
+  // input
+  const pizzaSize= parseInt(document.getElementById('pizza-Size').value)
+  const toppings = parseInt(document.getElementById('toppings').value)
+
+  // process
+  const pay = (pizzaSize + toppings) * (0.18) ;
+  const taxes = (HoursWorked * HourlyWage) *  0.18 ;
+
+  // output
+  document.getElementById('pay').innerHTML = `Your pay will be: $ ${pay.toFixed(2)}` ; 
+  document.getElementById('taxes').innerHTML = 'The  goverment will take: $' + taxes.toFixed(2) ; 
+}
