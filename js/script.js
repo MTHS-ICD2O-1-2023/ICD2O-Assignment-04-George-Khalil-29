053f9903858bf41acd2c62c5bfea3d17966d6c24
George Khalil <159185251+George-Khalil-29@users.noreply.github.com>
2024-05-03 03:02:24 +0000

    save

--- js/script.js
+++ js/script.js
@@ -10,18 +10,22 @@
  * This function calculates area of a triangle.
  */
 function calculateCost() {
-  const pizzaSizes = document.getElementById("pizzaSizes").value
+  const pizzaSizes = document.getElementById("pizzaSizes").checked
+  const extralarge = document.getElementById("extralarge").value;
   const toppingsCosts = document.getElementById("toppingsCosts").value
 
-  const Large1Topping = (6.00 + 1.00) * (1.0 - 0.18)
-   const Large2Topping = (6.0 + 1.75) * (1.0 - 0.18)
-  
-  if (pizzaSizes == "Large" || toppingsCosts == "1Topping") {
-    document.getElementById("Large1Topping").innerHTML =
-      "Your Pay will be:$" + (6.0 + 1.0) * (1.0 - 0.18)
-  }
-  else if (pizzaSizes == "Large" || toppingsCosts == "2Topping") {
-    document.getElementById(Large2Topping).innerHTML =
-      "Your pay will be:$" + (6.0 + 1.75) * (1.0 - 1.18)
-  }
- }
+  const orderSummary = (6.0 + 1.0) * (1.0 - 0.13)
+
+  if (pizzaSizes == "Large" || toppingsCosts == "1Topping" ) {
+    document.getElementById("orderSummary").innerHTML =
+      "Your pay will be:" + orderSummary + "$"
+  } else if (pizzaSizes == "Large" || toppingsCosts == "2Topping") {
+    document.getElementById("orderSummary").innerHTML =
+      "Your pay will be:" + (6.0 + 1.75) * (1.0 - 0.13) + "$"
+  } else if (pizzaSizes == "Large" || toppingsCosts == "3Topping") {
+    document.getElementById("orderSummary").innerHTML =
+      "Your pay will be:" + (6.0 + 2.5) * (1.0 - 0.13) + "$"
+  } else if (pizzaSizes == "Large" || toppingsCosts == "4Topping") {
+    document.getElementById("orderSummary").innerHTML = "Your pay will be:" + (6.0 + 3.35) * (1.0 - 0.13) +"$"
+  } 
+}
